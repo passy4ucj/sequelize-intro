bb1b09d8ae264d4c974112b848bdef04117df195
passy4ucj <ojinnnakapascal@gmail.com>
2021-02-23 16:55:00 +0100

    Create a new user function

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { User } = require('./models')
 
 // Creating an express app
 const app = express() 
@@ -11,6 +12,23 @@ app.get('/', (req, res) => {
     res.json({
         message: "Hello, World!"
     })
+})
+
+
+app.post('/user/create', async(req, res) => {
+    const { username } = req.body
+    try {
+        const isExist = await User.findAll({ where: {username} })
+        if(isExist.length > 0) return res.json({error: 'User already exists'})
+        const newUser = await User.create({username})
+        res.json({
+            message: `${newUser.username} created`
+        })
+    } catch (error) {
+        res.json({
+            error: 'Server Error'
+        })
+    }
 })
 
 
